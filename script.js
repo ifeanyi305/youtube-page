57ab97bbff7503fab8b3e4a114d863182c59c21c
unknown <otijoseph305@gmail.com>
2022-09-01 16:40:25 +0100

    first-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let openBtn = document.getElementById('open-btn');
+let modalContainer = document.getElementById('modal-container');
+let closeBtn = document.getElementById('close-btn');
+
+openBtn.addEventListener('click', function(){
+
+    modalContainer.style.display = "block";
+});
+
+closeBtn.addEventListener('click', function() {
+    modalContainer.style.display = "none";
+});
